64e9a9e6e404cf8de4e68611061c3a580766ccec
Med_Ali_Khaled <72525144+MEDALIALPHA331@users.noreply.github.com>
2023-02-25 16:08:15 +0100

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accentOrange: "hsl(25, 97%, 53%)",
+        primaryWhite: "hsl(0, 0%, 100%)",
+        lightGrey: "hsl(217, 12%, 63%)",
+        mediumGrey: "hsl(216, 12%, 54%)",
+        darkBlue: "hsl(213, 19%, 18%)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+
+      fontFamily: {
+        primary: ["Overpass", "sans-serif"],
+      },
+
+      fontWeight: {
+        mid: 400,
+        big: 700,
+      },
+
+      screens: {
+        mobile: "375px",
+        tablet: "768px",
+        desktop: "1440px",
+      },
+    },
   },
   plugins: [],
 };
